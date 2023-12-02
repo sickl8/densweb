80b1526314a3689dcec6970c8891493e6797136c
Ilyass Saadi <isaadi@c4r6p3.1337.ma>
2023-12-02 22:57:49 +0100

    added cursor to services

--- src/components/ServicesCarouselElement.svelte
+++ src/components/ServicesCarouselElement.svelte
@@ -27,7 +27,7 @@
 					rgba(var(--lavender_blush-rgb), 0%) 75%,
 					rgba(var(--lavender_blush-rgb), 0%) 100% -->
 
-<li class="-card-container -items --snap-center " data-scroll-snap-align="center" bind:this={carouselElements[index]}>
+<li class="-card-container cursor-pointer -items --snap-center " data-scroll-snap-align="center" bind:this={carouselElements[index]}>
 	<div class="--scale-[0.95] bg-black  rounded-xl border border-[#808080] overflow-hidden" style="width: {$serviceCardWidthInRems}rem">
 		<div class="-card h-64 py-6 px-8 flex flex-col gap-4" style="background: linear-gradient(190deg, {colors.white.replace("1)", "10%)")} 0%, {colors.white.replace("1)", "3%)")} 40%)">
 			<div class="-title flex-shrink flex gap-4 items-center">
